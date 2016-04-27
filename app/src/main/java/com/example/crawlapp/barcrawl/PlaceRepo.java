0ea2fb922d7e8a92f17028f88e27920c36f1683b
johnmiller86 <chefjohn2006@hotmail.com>
2016-04-27 17:45:24 -0400

    Initial commit

--- app/src/main/java/com/example/crawlapp/barcrawl/PlaceRepo.java
+++ app/src/main/java/com/example/crawlapp/barcrawl/PlaceRepo.java
@@ -1,7 +1,45 @@
 package com.example.crawlapp.barcrawl;
 
-/**
- * Created by chefj on 4/27/2016.
- */
-public class PlaceRepo {
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+
+public class PlaceRepo{
+
+    private Place place;
+
+    public PlaceRepo(){
+        place = new Place();
+    }
+
+
+    public static String createTable(){
+        return "CREATE TABLE " + Place.TABLE  + "("
+                + Place.KEY_PLACE_ID  + "   PRIMARY KEY,"
+                + Place.KEY_PLACE_NAME + " TEXT,"
+                + Place.KEY_PLACE_LAT + " TEXT,"
+                + Place.KEY_PLACE_LNG + " TEXT)";
+    }
+
+
+    public int insert(Place place) {
+        int placeId;
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
+        ContentValues values = new ContentValues();
+        values.put(Place.KEY_PLACE_ID, place.getPlaceId());
+        values.put(Place.KEY_PLACE_NAME, place.getPlaceName());
+        values.put(Place.KEY_PLACE_LAT, place.getPlaceLat());
+        values.put(Place.KEY_PLACE_LNG, place.getPlaceLng());
+
+        // Inserting Row
+        placeId = (int) db.insert(Place.TABLE, null, values);
+        DatabaseManager.getInstance().closeDatabase();
+
+        return placeId;
+    }
+
+    public void delete() {
+        SQLiteDatabase db = DatabaseManager.getInstance().openDatabase();
+        db.delete(Place.TABLE,null,null);
+        DatabaseManager.getInstance().closeDatabase();
+    }
 }
