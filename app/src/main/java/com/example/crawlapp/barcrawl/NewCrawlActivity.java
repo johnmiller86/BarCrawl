749dd3640b1a7515319f299e10ed006ccc9e8a5b
johnmiller86 <chefjohn2006@hotmail.com>
2016-04-27 20:04:47 -0400

    Initial commit

---
+++ app/src/main/java/com/example/crawlapp/barcrawl/NewCrawlActivity.java
@@ -0,0 +1,13 @@
+package com.example.crawlapp.barcrawl;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class NewCrawlActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_crawl);
+    }
+}
